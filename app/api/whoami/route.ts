4787c2f5199b724e59942945109074b699d4354a
iNikolas <realkolos1@gmail.com>
2024-06-21 15:56:56 +0300

    ip header

--- app/api/whoami/route.ts
+++ app/api/whoami/route.ts
@@ -1,10 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-export function GET(req: NextRequest) {
+export function GET(req: Request) {
   return NextResponse.json({
-    ipaddress: req.ip ?? null,
+    ipaddress:
+      req.headers.get("X-Real-Ip") ?? req.headers.get("X-Forwarded-For"),
     language: req.headers.get("Accept-Language"),
     software: req.headers.get("User-Agent"),
   });
